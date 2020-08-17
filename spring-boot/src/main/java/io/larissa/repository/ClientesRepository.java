51414944d3bad2991160f9c48a23a0c5f830e3e1
lari-sant <teixeiralary99@gmail.com>
2020-08-17 20:30:36 -0300

    Implementando uma REST API com Spring

---
+++ spring-boot/src/main/java/io/larissa/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.larissa.repository;
+
+import io.larissa.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base e salvar o cliente
+    }
+}
